e32a2573365c9bdede2082f96090119e2dc658d0
Sofiyana-1811 <ipsitajain18@gmail.com>
2023-08-25 21:46:42 +0530

    chunk

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,48 @@
-import Intro from "./Intro";
-import SlidingBar from "./SlidingBar";
+import localFont from "next/font/local";
+
+const dirtyline = localFont({src: "../../fonts/dirtyline.ttf"})
+// const aeonik = localFont({src: "../../fonts/Aeonik-Regular.ttf"})
 
 const LandingPage = () => {
     
     return (
-        <section id="home">
-            <div className="max-h-fit flex flex-col justify-center items-center mt-20 pl-[5.1rem] w-screen -z-20">
-                <SlidingBar />
-                <Intro />
+        <section id="home" className="flex flex-col justify-between px-10">
+            <div className="flex flex-col mt-auto">
+                <div className="flex flex-col" id="landing-text">
+                    <div className="flex flex-row justify-between items-end">
+                        <p className="mb-3">I'm</p>
+                        <p style={dirtyline.style} className="text-[4rem]">Digital</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-end">
+                        <p style={dirtyline.style} className="text-[4rem]">Bedanta</p>
+                        <p style={dirtyline.style} className="text-[4rem] text-[#FF4900]">Designer</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-end">
+                        <p style={dirtyline.style} className="text-[4rem]">Rick</p>
+                        {/* <div className="border-b-2 border-white w-96"></div> */}
+                        <p style={dirtyline.style} className="text-[4rem]">Creative Enthusiast</p>
+                    </div>
+                </div>
+                <p className="ml-auto text-[0.875rem] mr-1" id="landing-text">Still a student, in IIT Guwahati</p>
+                {/* <div className="flex flex-row justify-between items-end">
+                    <div className="flex flex-col items-start gap-0" id="landing-text">
+                        <p style={aeonik.style} className="mb-1">I'm</p>
+                        <p style={dirtyline.style} className="text-[4rem]">Bedanta</p>
+                        <p style={dirtyline.style} className="text-[4rem]">Rick</p>
+                    </div>
+                    <div className="flex flex-col items-end" id="landing-text">
+                        <p style={dirtyline.style} className="text-[4rem]">Digital</p>
+                        <p style={dirtyline.style} className="text-[4rem]">Designer</p>
+                        <p style={dirtyline.style} className="text-[4rem]">Creative Enthusiast</p>
+                    </div>
+                </div> */}
+                <svg width="45" height="66" viewBox="0 0 45 66" className="scale-75 ml-auto my-10 mr-2">
+                    <rect x="15.9999" width="12" height="65" fill="#FF4900"/>
+                    <rect x="35.5565" y="41" width="12" height="22" transform="rotate(45 35.5565 41)" fill="#FF4900"/>
+                    <rect width="12" height="10.7347" transform="matrix(-0.707107 0.707107 0.707107 0.707107 8.48523 41)" fill="#FF4900"/>
+                </svg>
             </div>
+            
         </section>
         
     )
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,36 +1,78 @@
 import { Collapse } from "react-collapse";
 
-export default function ProjectCard({open, toggle, year, title, ref_link, desc, tags }) {
+export default function ProjectCard({ num1, year1, title1, image1, tags1, ref_link1, num2, year2, title2, image2, tags2, ref_link2  }) {
+    const exp_num1 = "EXP. "+num1;
+    const exp_num2 = "EXP. "+num2;
+    const isReverse = parseInt(num1)%4===3?true:false;
     
     return (
-        <div className="w-[87%] h-fit ml-28 mr-14 rounded-lg border border-[#2B0C7D] dark:border-[#F5F5F5] pr-4 py-4">
-            <div className="flex flex-row justify-between items-center">
-                <div className="flex flex-row items-center" onClick={toggle}>
-                    <span className="text-sm mx-4">
-                        {year}
-                    </span>
-                    <span className="text-lg">
-                        {title}
-                    </span>
+        <div className={`flex flex-${isReverse?'row-reverse':'row'} justify-between`}>
+            <div className={`bg-${image1} bg-cover flex flex-col justify-between relative bg-[#D9D9D9] h-96 rounded-2xl p-6 w-[55%]`}>
+                <div className="flex flex-row justify-between items-start">
+                    <div className="flex flex-row gap-2 font-bold">
+                        <span className="text-[#121212]">{exp_num}</span>
+                        <div className="inline-block w-0.5 self-stretch bg-[#FF4900]"></div>
+                        <span className="text-[#868686]">{year}</span>
+                    </div>
+                    <svg width="48" height="48" viewBox="0 0 48 48">
+                        <circle cx="24" cy="24" r="24" fill="#121212"/>
+                        {/* <path d="M32.1703 10.7234H10.7235V18.383H24.2554L10.7235 31.6596L16.3405 37.0213L37.2767 15.8298L32.1703 10.7234Z" fill="#FFF8F8"/>
+                        <path d="M29.6171 37.0213V24.2553L37.2767 31.6596V37.0213H29.6171Z" fill="#FFF8F8"/> */}
+                        <path d="M28.3077 17H17V21.0777V28.1456V31L26.9615 31H31V28.1456V19.7184V17H28.3077Z" fill="#FFF8F8"/>
+                    </svg>
                 </div>
-                <div className="flex flex-row gap-2">
-                    {tags.map((tag, index) => (
-                        <div className="bg-inherit font-semibold text-sm px-3 py-2 border rounded-[1.875rem] border-[#2B0C7D] dark:border-[#F5F5F5]">
-                            {tag}
-                        </div>
-                    ))}
-                </div>
-            </div>
-            
-            <Collapse isOpened={open}>
-                <div className="flex flex-row items-center py-2">
-                    <div className="ml-11 mr-4 inline-block w-[1px] self-stretch bg-[#2B0B7D] dark:bg-white"></div>
-                    <div className="w-[60%]">
-                        {desc}
+                <div className="flex flex-col gap-2">
+                    <div className="font-bold text-2xl">
+                        {title1}
+                    </div>
+                    <div className="flex flex-row gap-2 justify-start items-center font-medium">
+                        {tags1.map((tag, index) => (
+                            <div>
+                                <div>
+                                    {tag}
+                                </div>
+                            
+                                {index<tags1.length-1&&<svg width="4" height="4" viewBox="0 0 4 4">
+                                    <circle cx="2" cy="2" r="2" fill="#121212"/>
+                                </svg>}
+                            </div>
+                        ))}
                     </div>
                 </div>
-            </Collapse>
-           
+            </div>
+            <div className={`bg-${image2} bg-cover flex flex-col justify-between relative bg-[#D9D9D9] h-96 rounded-2xl p-6 w-[43%]=`}>
+                <div className="flex flex-row justify-between items-start">
+                    <div className="flex flex-row gap-2 font-bold">
+                        <span className="text-[#121212]">{exp_num}</span>
+                        <div className="inline-block w-0.5 self-stretch bg-[#FF4900]"></div>
+                        <span className="text-[#868686]">{year}</span>
+                    </div>
+                    <svg width="48" height="48" viewBox="0 0 48 48">
+                        <circle cx="24" cy="24" r="24" fill="#121212"/>
+                        {/* <path d="M32.1703 10.7234H10.7235V18.383H24.2554L10.7235 31.6596L16.3405 37.0213L37.2767 15.8298L32.1703 10.7234Z" fill="#FFF8F8"/>
+                        <path d="M29.6171 37.0213V24.2553L37.2767 31.6596V37.0213H29.6171Z" fill="#FFF8F8"/> */}
+                        <path d="M28.3077 17H17V21.0777V28.1456V31L26.9615 31H31V28.1456V19.7184V17H28.3077Z" fill="#FFF8F8"/>
+                    </svg>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <div className="font-bold text-2xl">
+                        {title2}
+                    </div>
+                    <div className="flex flex-row gap-2 justify-start items-center font-medium">
+                        {tags2.map((tag, index) => (
+                            <div>
+                                <div>
+                                    {tag}
+                                </div>
+                            
+                                {index<tags2.length-1&&<svg width="4" height="4" viewBox="0 0 4 4">
+                                    <circle cx="2" cy="2" r="2" fill="#121212"/>
+                                </svg>}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
