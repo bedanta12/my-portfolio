e274c12a244156b3f8f6d884fe78d8d7430b084c
Yuvraj <yyuvraj54@gmail.com>
2023-10-12 15:57:22 +0530

    Added a Loading Screen

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,23 @@ import SlidingBar from '@/components/SlidingBar'
 import Slide from '@mui/material/Slide';
 import { usePopup } from '@/components/PopupContext';
 import ContactPageSmall from '@/components/ContactPageSmall'
-
+import LoadingScreen from '@/components/LoadingPage';
+import React, { useState, useEffect } from 'react';
 export default function Home() {
   const { isPopupOpen, togglePopup } = usePopup();
+  const [isLoading, setIsLoading] = useState(true);
   
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); 
+  }, []);
+
   return (
     <main className="text-[#121212] dark:text-[#EAEAEA] bg-bg1 bg-no-repeat dark:bg-bg2 dark:bg-no-repeat relative">
+      {isLoading ? (  
+        <LoadingScreen />
+      ) : (
       <div className="bg-[#DED5D4] bg-opacity-50 dark:bg-[#303030] dark:bg-opacity-50 overflow-y-scroll scrollbar-hide mx-3 md:mx-6">
         <Navbar />
         {/* <ChatPage /> */}
@@ -22,6 +33,7 @@ export default function Home() {
         <SlidingBar/>
         <Footer/>
       </div>
+       )}
       <Slide direction="up" in={isPopupOpen} mountOnEnter unmountOnExit>
         <div className="sticky bottom-0 left-0 right-0">
           <ContactPage/>
--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,10 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+import styles from 'src/app/globals.css'; // Import your CSS module
+const imagePaths = ['/load1.png', '/load2.png', '/load3.png']; // Add the paths to your images
+
 const LoadingPage = () => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        // Move to the next image in the sequence
+        setCurrentImageIndex((prevIndex) =>
+          prevIndex === imagePaths.length - 1 ? 0 : prevIndex + 1
+        );
+      }, 500); // Change images every 1 second
+  
+      return () => clearInterval(interval);
+    }, []);
+  
     return (
-        <div className="w-screen h-screen flex items-center">
-
+      <div className="fullScreen">
+        <div className="centered">
+          <img
+            src={imagePaths[currentImageIndex]}
+            alt="Loading"
+            className="image" // Apply a class to set the image size
+          />
         </div>
-    )
-
+      </div>
+    );
   };
   
   export default LoadingPage;
