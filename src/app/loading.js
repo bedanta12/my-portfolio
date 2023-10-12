985797d09156b156865441864d5245a928399818
Arjun Singh <117923579+aarjun8060@users.noreply.github.com>
2023-10-12 13:55:29 +0530

    feat/loader in homepage

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,4 +1,26 @@
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return "loading ..."
-  }
+import Image from "next/image";
+
+const Loading = () => {
+    return (
+        <div className="w-screen h-screen flex items-center  justify-center bg-black z-50">
+            
+                    <div> 
+                    <Image
+
+                        src="/loader.gif"
+                        loading="lazy"
+                        alt="loader"
+                  
+                        width={30}
+                        height={30}
+                        className=" !static"
+
+                    />
+                    </div>
+               
+        </div>
+    )
+
+  };
+  
+  export default Loading;
