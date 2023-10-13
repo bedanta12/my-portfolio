7ee3548e96cae55bfefcb99928209dbde641ea7b
Dhruv127 <dhruvqagarwal@gmail.com>
2023-10-13 13:56:32 +0530

    add-feature-contact-page

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,37 @@
-'use client'
-import Footer from '@/components/Footer'
-import LandingPage from '@/components/LandingPage'
-import Navbar from '@/components/Navbar'
-import ProjectsPage from '@/components/ProjectsPage'
-import ContactPage from '@/components/ContactPage'
-import SlidingBar from '@/components/SlidingBar'
+'use client';
+import React, { useLayoutEffect } from 'react';
+import Footer from '@/components/Footer';
+import LandingPage from '@/components/LandingPage';
+import Navbar from '@/components/Navbar';
+import ProjectsPage from '@/components/ProjectsPage';
+import ContactPage from '@/components/ContactPage';
+import SlidingBar from '@/components/SlidingBar';
 import Slide from '@mui/material/Slide';
 import { usePopup } from '@/components/PopupContext';
-import ContactPageSmall from '@/components/ContactPageSmall'
+import ContactPageSmall from '@/components/ContactPageSmall';
 
 export default function Home() {
   const { isPopupOpen, togglePopup } = usePopup();
-  
+
+  useLayoutEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (isPopupOpen && !document.getElementById('contact-fancy-text').contains(e.target)) {
+        togglePopup();
+        document.querySelector("body").style.overflow = "auto";
+      }
+    };
+
+    if (isPopupOpen) {
+      document.querySelector("body").style.overflow = "hidden";
+      document.addEventListener('click', handleOutsideClick, true);
+    }
+
+    return () => {
+      document.querySelector("body").style.overflow = "auto";
+      document.removeEventListener('click', handleOutsideClick, true);
+    };
+  }, [isPopupOpen, togglePopup]);
+
   return (
     <main className="text-[#121212] dark:text-[#EAEAEA] bg-bg1 bg-no-repeat dark:bg-bg2 dark:bg-no-repeat relative">
       <div className="bg-[#DED5D4] bg-opacity-50 dark:bg-[#303030] dark:bg-opacity-50 overflow-y-scroll scrollbar-hide mx-3 md:mx-6">
@@ -19,19 +39,14 @@ export default function Home() {
         {/* <ChatPage /> */}
         <LandingPage />
         <ProjectsPage />
-        <SlidingBar/>
-        <Footer/>
+        <SlidingBar />
+        <Footer />
       </div>
       <Slide direction="up" in={isPopupOpen} mountOnEnter unmountOnExit>
-        <div className="sticky bottom-0 left-0 right-0">
-          <ContactPage/>
+        <div id="contact-fancy-text" className="sticky bottom-0 left-0 right-0">
+          <ContactPage />
         </div>
       </Slide>
-      {/* {window?.innerWidth<768&&<Slide direction="up" in={isPopupOpen} mountOnEnter unmountOnExit>
-        <div className="sticky bottom-0 left-0 right-0">
-          <ContactPageSmall/>
-        </div>
-      </Slide>} */}
     </main>
-  )
+  );
 }
