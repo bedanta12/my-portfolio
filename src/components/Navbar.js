1dc151d7cc44fb295427bbf474541a25438ea2ac
Dhruv127 <dhruvqagarwal@gmail.com>
2023-10-17 01:54:15 +0530

    fix-menu-feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { usePopup } from './PopupContext';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import Collapse from 'react-collapse';
+import { RiCloseLine } from 'react-icons/ri';
 
 const Navbar = () => {
   const { isPopupOpen, togglePopup } = usePopup();
@@ -34,28 +35,33 @@ const Navbar = () => {
           Resume
         </a>
       </div>
-      <div className='flex flex-col md:hidden text-[16px]'>
-        <span onClick={toggleMenu}>Menu</span>
-        <Collapse isOpened={isOpen} className="z-30 absolute right-0 top-10">
-        <ul>
-          <li>
-          <button onClick={togglePopup}>
-          Contact
-        </button>
+      <div className="flex flex-col md:hidden text-[16px] mt-5">
+      <span onClick={toggleMenu} className="cursor-pointer self-end">
+        {isOpen ? (
+          <span className="flex items-center mb-1">
+            <RiCloseLine size={20} />
+            <span className="ml-1">Close</span>
+          </span>
+        ) : (
+          'Menu'
+        )}
+      </span>
+      <Collapse isOpened={isOpen} className="z-30 absolute right-0 top-12">
+        <ul className="flex flex-col items-end">
+          <li className="mb-1">
+            <button onClick={togglePopup}>Contact</button>
           </li>
-          <li>
-          <Link href='/knowme'>
-          Know Me!
-        </Link>
+          <li className="mb-1">
+            <Link href="/knowme">Know Me!</Link>
           </li>
-          <li>
-            <a href='https://drive.google.com/file/d/1teguoPNWgoDj3p1hDeEX_PcjqBuPF7NN/view?usp=drive_link' target='_blank'>
-          Resume
-        </a>
-        </li>
+          <li className="mb-1">
+            <a href="https://drive.google.com/file/d/1teguoPNWgoDj3p1hDeEX_PcjqBuPF7NN/view?usp=drive_link" target="_blank">
+              Resume
+            </a>
+          </li>
         </ul>
-        </Collapse>
-      </div>
+      </Collapse>
+    </div>
     </nav>
     
     );
