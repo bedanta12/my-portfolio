00d9e22c8e3c92a097b66e1f9048c3849d7301e3
Dhruv127 <dhruvqagarwal@gmail.com>
2023-10-17 17:49:23 +0530

    fix-menu-feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,8 +14,8 @@ const Navbar = () => {
   };
   
   return (    
-    <nav className="flex flex-row justify-between pt-1 pr-4 md:p-4 items-center fixed left-3 right-3 md:left-6 top-0 md:right-6 z-20 overflow-hidden scrollbar-hide backdrop-blur">
-      <Link href='/'>
+    <nav className="flex flex-row items-center justify-between pt-2 pr-4 md:p-4 fixed left-3 right-3 md:left-6 top-0 md:right-6 z-20 overflow-hidden scrollbar-hide backdrop-blur">
+      <Link href='/' style={{alignSelf:"start"}} >
         <svg width="72" height="40" viewBox="0 0 72 40" className="dark:stroke-[#FFF8F8] stroke-[#121212] fill-transparent scale-[60%] md:scale-100 md:w-[72px] md:h-[40px]">
           <rect x="16" y="14" width="16" height="16" rx="8" className='dark:fill-[#FFF8F8] fill-[#121212]'/>
           <rect x="48" y="14" width="16" height="16" rx="8" className='dark:fill-[#FFF8F8] fill-[#121212]'/>
@@ -23,7 +23,7 @@ const Navbar = () => {
           <rect x="1" y="1" width="70" height="38" rx="19" stroke-width="2"/>
         </svg>
       </Link>
-      <ThemeButton/>
+      <ThemeButton  className="inline-block" style={{ width: "10px" }}/>
       <div className="hidden flex-row gap-6 font-medium pr-6 md:flex">
         <button onClick={togglePopup}>
           Contact
@@ -35,32 +35,47 @@ const Navbar = () => {
           Resume
         </a>
       </div>
-      <div className="flex flex-col md:hidden text-[16px] mt-5">
-      <span onClick={toggleMenu} className="cursor-pointer self-end">
+      <div className="flex flex-col md:hidden text-[16px] mt-5 ">
+      <span onClick={toggleMenu} className="cursor-pointer self-end mb-4">
         {isOpen ? (
-          <span className="flex items-center mb-1">
-            <RiCloseLine size={20} />
-            <span className="ml-1">Close</span>
+          <span className="flex items-center">
+            <span className="mr-1">Close</span>
+            <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="20"
+  height="20"
+  viewBox="0 0 20 20"
+  fill="none"
+  stroke="currentColor"
+  strokeWidth="2"
+  strokeLinecap="round"
+  strokeLinejoin="round"
+>
+  <line x1="4" y1="4" x2="16" y2="16" />
+  <line x1="16" y1="4" x2="4" y2="16" />
+</svg>
+
           </span>
         ) : (
           'Menu'
         )}
       </span>
       <Collapse isOpened={isOpen} className="z-30 absolute right-0 top-12">
-        <ul className="flex flex-col items-end">
-          <li className="mb-1">
-            <button onClick={togglePopup}>Contact</button>
-          </li>
-          <li className="mb-1">
-            <Link href="/knowme">Know Me!</Link>
-          </li>
-          <li className="mb-1">
-            <a href="https://drive.google.com/file/d/1teguoPNWgoDj3p1hDeEX_PcjqBuPF7NN/view?usp=drive_link" target="_blank">
-              Resume
-            </a>
-          </li>
-        </ul>
-      </Collapse>
+  <ul className="flex flex-col items-end justify-center">
+     <li className="mb-3">
+      <Link href="/knowme">Know Me!</Link>
+    </li>
+    <li className="mb-3">
+      <button type="button" onClick={togglePopup}>Contact</button>
+    </li>
+    <li className="mb-3">
+      <a href="https://drive.google.com/file/d/1teguoPNWgoDj3p1hDeEX_PcjqBuPF7NN/view?usp=drive_link" target="_blank">
+        Resume
+      </a>
+    </li>
+  </ul>
+</Collapse>
+
     </div>
     </nav>
     
